90b240080b56553603f17d45cb918c360f3878a3
jord2097 <100094374+jord2097@users.noreply.github.com>
2022-03-11 15:22:36 +0000

    version as presented, enemy health broken

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 class Room {
-    constructor(name, description, requirement, failText, item){
+    constructor(name, description){
         this._name = name;
         this._description = description;
-        this._requirement = requirement // item name for keys, defeat a certain enemy etc
-        this._linkedRooms = {};
-        this._failText = failText;
-        this._item = item
+        //this._requirement = requirement // item name for keys, defeat a certain enemy etc
+        this._linkedRooms = {};        
     }
 
     get name(){
@@ -57,37 +55,47 @@ class Room {
 }
 
 class Hero {
-    constructor(health, stamina, strength, inventory = [], weapon){
+    constructor(health, maxHealth, attack, inventory = [], weapon, gold){
         this._health = health
-        this._stamina = stamina
-        this._strength = strength
+        this._maxHealth = maxHealth
+        this._attack = attack
         this._inventory = inventory
-        this.weapon = weapon
+        this._weapon = weapon
+        this._gold = gold 
     }
 
     get health(){
         return this._health
     }
 
-    get stamina(){
-        return this._stamina
+    get maxHealth(){
+        return this._maxHealth
     }
 
-    get strength(){
-        return this._strength
+    get attack(){
+        return this._attack
     }
 
     get inventory(){
         return this._inventory
     }
 
+    get weapon(){
+        return this._weapon
+    }
+
+    set weapon(name){
+        this._weapon = name
+    }
+
     addItems(item){
         this._inventory = this._inventory.concat(item);
         return this._items;
     }
+    
 }
 
-class Character {
+/* class Character {
     constructor(name, description, conversation) {
         this._name = name;
         this._description = description;
@@ -124,16 +132,15 @@ class Character {
 
     
 
-}
+} */
 
-class Enemy extends Character{
-    constructor(name, description, conversation, item, physWeakness, pwMultiplier, health, stamina){
-        super(name, description, conversation)
-        this._item = item
-        this._physWeakness = physWeakness
-        this._pwMultiplier = pwMultiplier // how much damage increases if enemy is weak to attack
-        this._health = health
-        this._stamina = stamina
+class Enemy extends Hero{
+    constructor(name, description, health, maxHealth, attack, goldgain){
+        super(health, maxHealth, attack)
+        this._name = name
+        this._description = description
+        this._goldgain = goldgain
+
     }
 
     get item(){
@@ -155,10 +162,12 @@ class Enemy extends Character{
     get stamina() {
         return this._stamina
     }
+
+    
     
 }
 
-class Weapon {
+/* class Weapon {
     constructor(name, description, attackType){
         this._name = name
         this._description = description
@@ -171,39 +180,54 @@ class Weapon {
         return this._name + ". " + this._description;
     }
     
-    // function to attack using weapon
+    
+}  */
 
-} // choice of weapon at start
+const heroIG = new Hero(100, 100, 20, [], null, 0 )
+const Undead = new Enemy("Zombie", "Once a human, this undead creature must have succumbed to some kind of virus.", 50, 50, 10, 50)
+const UndeadDog = new Enemy("Zombie Dog", "This dog is infected much like the zombies, but you wonder whether it was subjected to the virus on purpose.", 25, 25, 12, 40)
+const PoisonUndead = new Enemy("Poison Zombie", "This zombie seems to have mutated beyond the infection, as it is spewing out a cloud of toxic gas.", 35, 35, 18, 60)
+const Wendigo = new Enemy("Wendigo", "An evil spirit with a heart of ice. Good luck defeating this one.", 500, 500, 50, 1000)
+// const BurningBoss = new Enemy ("Burning Zombie", "This zombie is engulfed in flames, although it seems to be able to survive the pain. Don't let it get too close! ", 20, 20, 15, 100)
 
-const mainHall = new Room("Main Hall", "This is a large but dingy entrance area with flickering lights and an ominous presence. There are many directions to go, but you wonder how easy it might be to get lost.", null, null, "Blue Key")
-const mainHallUpper = new Room("Main Hall Upper Area", "After going up the stairs, you are now on the landing of the 1st floor. There's not much to see up here, except a collection of abstract paintings and a door on each end.", null, null, )
-const diningRoom = new Room("Dining Room", "Plenty of scran was eaten here.")
+const mainHall = new Room("Main Hall", "This is a large but dingy entrance area with flickering lights and an ominous presence. There are many directions to go, but you wonder how easy it might be to get lost.")
+const mainHallUpper = new Room("Main Hall Upper Area", "After going up the stairs, you are now on the landing of the 1st floor. There's not much to see up here, except a collection of abstract paintings and a door on each end.")
+const diningRoom = new Room("Dining Room", "Plenty of candlelit dinners were eaten here.")
+const Kitchen = new Room("Kitchen", "A large kitchen with many cupboards to hide in.")
+const Bedroom = new Room("Master Bedroom", "A gigantic bedroom filled with luxurious decorations.")
+const Bathroom = new Room("Bathroom", "A disgusting toilet, this hasn't been cleaned in years.")
 
 mainHall.linkRoom("upstairs", mainHallUpper)
 mainHall.linkRoom("west", diningRoom)
+mainHall.linkRoom("east", Kitchen)
+Kitchen.linkRoom("west", mainHall)
 diningRoom.linkRoom("east", mainHall)
 mainHallUpper.linkRoom("downstairs", mainHall)
+mainHallUpper.linkRoom("west", Bedroom)
+mainHallUpper.linkRoom("east", mainHallUpper)
+Bedroom.linkRoom("east", mainHallUpper)
+Bathroom.linkRoom("west", mainHallUpper)
 
-const claymore = new Weapon("Claymore","A Scottish two-handed medieval sword.","greatsword", "swing")
+/* const sword = new Weapon("Claymore","A Scottish two-handed medieval sword.","greatsword", "swing")
 const battleaxe = new Weapon("Battle Axe", "A two-handed axe specifically designed for combat.", "swing")
 const pike = new Weapon("Pike", "A long pole-like spear that is wielded two-handed", "thrust")
-const twindaggers = new Weapon("Twin Daggers", "Two short daggers that aim for speed over damage.", "slash")
-const weapons = [claymore, battleaxe, pike, twindaggers]
+const daggers = new Weapon("Twin Daggers", "Two short daggers that aim for speed over damage.", "slash")
+const weapons = [sword, battleaxe, pike, daggers] */
 
 function display(room){
     const textContent = room.describe();
     document.getElementById("textArea").innerHTML = textContent
     document.getElementById("userInput").focus();
-}
+} // displays description of current room
 
 function startGame(){
+    document.getElementById("fightInput").style.display = "none"
     currentRoom = mainHall;
-    display(currentRoom)
-    let currWeapon = 0
+    display(currentRoom)   
+    roomListener()
+} // establishes start point
 
-      
-    
-
+function roomListener(){
     document.addEventListener("keydown", function (event){
         if(event.key === "Enter"){
             const command = document.getElementById("userInput").value.toLowerCase();
@@ -212,6 +236,7 @@ function startGame(){
                 currentRoom = currentRoom.move(command)
                 console.log(currentRoom)
                 display(currentRoom)
+                preFight()
                 document.getElementById("userInput").value = ""                
             } else {
                 document.getElementById("userInput").value = ""
@@ -219,35 +244,123 @@ function startGame(){
             }
         }
     })
+} // listens for room change commands
+
+function preFight(){
+    const enemies = [
+        {name: "null", pct: 45},
+        {name: "Undead", const: Undead, pct: 25},
+        {name: "UndeadDog", const: UndeadDog, pct: 15},
+        {name: "PoisonUndead", const: PoisonUndead, pct: 10},
+        {name: "Wendigo", const: Wendigo, pct: 5}
+    ];
+    const expanded = enemies.flatMap(enemy => Array(enemy.pct).fill(enemy))
+    const winner = expanded[Math.floor(Math.random() * expanded.length)];
+    console.log("Enemy Found: " + winner.name)
+    // call fight function on const on winner    
+    if (winner.name != "null" ){
+        fight(winner.const)
+
+    } 
+
+} // randomly spawns enemy based on percentage chance
+
+function fight(enemyPresent){
+    document.getElementById("fightInfo").innerHTML = "Upon entering this room you have encountered a " + enemyPresent._name + ". " + enemyPresent._description
+    document.getElementById("fightStatus").innerHTML = "Your Health: " + heroIG._health + "/" + heroIG._maxHealth + " Enemy Health: " + enemyPresent._health + "/" + enemyPresent._maxHealth
+    document.getElementById("fightOptions").innerHTML = "Your options: <br> 1: Attack for an average of " + heroIG._attack + " damage. <br> 2: Run"
+    document.getElementById("userInput").style.display = "none"
+    document.getElementById("fightInput").style.display = "block"
+    document.addEventListener("keydown", function (event){
+        if (event.key === "Enter"){
+            const command = parseInt(document.getElementById("fightInput").value)            
+            if(command === 1){
+                attack(enemyPresent)
+            } else if (command === 2){
+                document.getElementById("userInput").style.display = "block"
+                document.getElementById("fightInput").style.display = "none"
+                document.getElementById("fightInfo").innerHTML = "You chose to run, type the direction you want to run."
+
+            }
+        }
+    })
+
+
+
+} // begins confrontation with enemy, option to run or attack
+
+function attack(enemyPresent){
+    const pAttack = [
+        {name: "normal attack", attack: heroIG._attack, pct: 70},
+        {name: "missed attack", attack: heroIG._attack / 2, pct: 25},
+        {name: "critical attack", attack: heroIG._attack * 1.5, pct: 5}        
+    ]
+    const expanded = pAttack.flatMap(user => Array(user.pct).fill(user))
+    const winner = expanded[Math.floor(Math.random() * expanded.length)]
+    const pAttackRandom = winner.attack
+    console.log(enemyPresent._health)
+
+    enemyPresent._health -= pAttackRandom
+    heroIG._health -= enemyPresent._attack    
+    
+    document.getElementById("fightDamage").innerHTML = "You did " + winner.attack + " damage to the " + enemyPresent._name + "! In return the enemy has done " + enemyPresent._attack + " damage to you."
+    document.getElementById("fightStatus").innerHTML = "Your Health: " + heroIG._health + "/" + heroIG._maxHealth + " Enemy Health: " + enemyPresent._health + "/" + enemyPresent._maxHealth
+
+    if (enemyPresent._health <= 0){
+        heroIG._gold += enemyPresent._goldgain;
+        document.getElementById("fightDamage").innerHTML = "You killed the " + enemyPresent._name + "! As a result you got " + enemyPresent._goldgain + " gold!" + " Now you can move to another room safely."
+        document.getElementById("fightInput").style.display = "none"
+        document.getElementById("userInput").style.display = "block"
+        if(heroIG._gold >= 200){
+            gameWon()
+        }
+
+
+        
+    }    
+    if (heroIG._health <= 0){
+        gameOver()
+    }
+} // combat system with varying damage
+
+function gameOver(){
+    document.getElementById("fightDamage").innerHTML = "You have been defeated by the enemy! You must refresh to try again."
 }
 
-function initDisplay(){
+function gameWon(){
+    document.getElementById("fightDamage").innerHTML = "You have successfully gained over 200 gold after defeating the enemy. You win!"
+}
+
+/* function weaponChoice(){
     document.getElementById("textArea").innerHTML = "You wake up outside an ominous mansion with no civilisation around. You see a bundle of weapons on the ground, you will need one if you are to enter this house. Your options are:"
     document.getElementById("weaponChoice1").innerHTML = "1: " + claymore.describe()
     document.getElementById("weaponChoice2").innerHTML = "2: " + battleaxe.describe()
     document.getElementById("weaponChoice3").innerHTML = "3: " + pike.describe()
     document.getElementById("weaponChoice4").innerHTML = "4: " + twindaggers.describe()
+       
 
     document.addEventListener("keydown", function (event){
         if(event.key === "Enter"){
             const command = parseInt(document.getElementById("userInput").value);
             const values = [0,1,2,3]
             if(values.includes(command)){
-                Hero.weapon = weapons[command - 1]
-                console.log(Hero.weapon)
+                heroIG.weapon = weapons[command - 1]
+                console.log(heroIG.weapon)
                 document.getElementById("userInput").value = ""
                 document.getElementById("weaponChoice1").style.display = "none";
                 document.getElementById("weaponChoice2").style.display = "none";
                 document.getElementById("weaponChoice3").style.display = "none";
-                document.getElementById("weaponChoice4").style.display = "none";
+                document.getElementById("weaponChoice4").style.display = "none";                
                 startGame()
+                return;
             } else {
                 document.getElementById("userInput").value = ""
                 alert("Not a valid choice.")
 
             }
+            return;
         }
-    })
+    }, {once: true})
 
     
 
@@ -255,4 +368,12 @@ function initDisplay(){
 
 }
 
-initDisplay()
+initDisplay() */
+// gave user a choice of weapons
+
+
+startGame()
+
+
+
+
